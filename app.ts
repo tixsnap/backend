5348b39c46fe5cf87975fb24e871f5c08c86fadb
Muhamad Gatot Supiadin <gatotbima2002@gmail.com>
2025-01-22 11:07:26 +0700

    feat: setup-structured-file

---
+++ app.ts
@@ -0,0 +1,47 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+export class App {
+  private app: Application;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  // configuration express
+  private configure() {
+    this.app.use(express.json());
+    // this.app.use(express.urlencoded({extended: true}));
+    this.app.use(cors());
+  }
+
+  // routes configuration
+  private routes() {}
+
+  // handler configuration
+  private handleError() {
+    // Handle route unknown
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      res.status(404).send("Not Found !");
+    });
+
+    // Handle Error
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        res.status(500).send({
+          message: err.message,
+        });
+      }
+    );
+  }
+
+  // start func
+  start() {
+    this.app.listen(8080, () => {
+      console.log(`Server running on PORT 8080`);
+    });
+  }
+}
---
+++ prisma/migrations/20250121170650_add_is_used_couponrefferal_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "coupon_referrals" ADD COLUMN     "isUsed" BOOLEAN NOT NULL DEFAULT false;
